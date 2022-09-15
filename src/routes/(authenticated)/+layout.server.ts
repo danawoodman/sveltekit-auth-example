fa7d86076969cc6fb470ba046d8f733135cf3178
Dana Woodman <dana@danawoodman.com>
2022-09-14 23:24:41 -0700

    Only return id and email in API endpoint

--- src/routes/(authenticated)/+layout.server.ts
+++ src/routes/(authenticated)/+layout.server.ts
@@ -4,5 +4,5 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async (event) => {
 	const user = event.locals?.user;
 	if (!user) throw redirect(301, "/login");
-	return { user };
+	return { user: { id: user.id, email: user.email } };
 };
