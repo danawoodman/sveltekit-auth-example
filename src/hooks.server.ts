524e6ffc45e3b04e778e83d9b976fed746fc2aea
Dana Woodman <dana@danawoodman.com>
2022-09-15 15:55:18 -0700

    Support Authorization header for API requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,19 @@
+import { find_by_token } from "$lib/auth";
 import type { Handle } from "@sveltejs/kit";
-import debug from "debug";
-import { find_by_token, login } from "$lib/auth";
-
-const log = debug("app:hooks.server");
 
 export const handle: Handle = async ({ event, resolve }) => {
-	log("url:", event.url);
-
-	const token = event.cookies.get("auth_token");
+	// Grab the auth_token from the cookies for non-API request:
+	const cookie_token = event.cookies.get("auth_token") as string;
+	// Grab the `Authorization: Bearer <token>` header for API requests:
+	const bearer_token = event.request.headers
+		.get("Authorization")
+		?.split(" ")[1];
+	const token = cookie_token ?? bearer_token;
 
 	if (token) {
 		const user = await find_by_token(event.cookies, token);
 		if (user) event.locals.user = user;
-	} else event.cookies.delete("auth_token");
+	}
 
 	return resolve(event);
 };
