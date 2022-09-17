6fd9d2a7fb3e14fc2660c2e224f7b3755c7ca5d0
Dana Woodman <dana@danawoodman.com>
2022-09-17 09:55:33 -0700

    Make const a number

--- src/lib/constants.server.ts
+++ src/lib/constants.server.ts
@@ -1,5 +1,6 @@
 import { env } from "$env/dynamic/private";
 const one_day = 60 * 60 * 24;
 
-export const AUTH_TOKEN_EXPIRY_SECONDS =
-	env?.AUTH_TOKEN_EXPIRY_SECONDS ?? one_day * 365;
+export const AUTH_TOKEN_EXPIRY_SECONDS = Number(
+	env?.AUTH_TOKEN_EXPIRY_SECONDS ?? one_day * 365
+);
