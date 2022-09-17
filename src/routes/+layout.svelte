f43946de82b26d6a8a72c2190d87facfb3339f1b
Dana Woodman <dana@danawoodman.com>
2022-09-17 09:58:01 -0700

    Log page data in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import "../app.postcss";
+	import type { LayoutData } from "./$types";
 	import { page } from "$app/stores";
 	import Footer from "./Footer.svelte";
 	import Header from "./Header.svelte";
+	import debug from "debug";
+
+	const log = debug("app:routes:layout.svelte");
+
+	export let data: LayoutData;
 
 	$: title = $page.data?.title ? $page.data.title + " | " : "";
+
+	$: log("data:", data);
+	$: log("$page.data:", $page.data);
 </script>
 
 <svelte:head>
